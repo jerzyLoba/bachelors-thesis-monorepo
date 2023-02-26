26a2da029ad9b36be3585ad55c8379b95a855a73
Jerzy Łoba <jloba@protonmail.com>
2023-02-27 00:50:52 +0100

    feat: add device id middleware

--- service-gateway/src/index.ts
+++ service-gateway/src/index.ts
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 
 import { mainRouter } from "./routes";
+import { deviceIdentification } from "./middlewares";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(deviceIdentification);
 app.use(mainRouter);
 
 app.listen(port, () => {
