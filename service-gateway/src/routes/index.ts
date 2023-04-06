a196496bb172fe1a5cc2c9557b60acfeaab84065
Jerzy Łoba <jloba@protonmail.com>
2023-04-06 02:28:30 +0200

    feat(gateway): implement gateway to product service

--- service-gateway/src/routes/index.ts
+++ service-gateway/src/routes/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { userRouter } from "./user";
+import { productsRouter } from "./products";
 
 const router = express.Router();
 
 router.use("/user", userRouter);
+router.use("/products", productsRouter);
 
 export { router as mainRouter };
