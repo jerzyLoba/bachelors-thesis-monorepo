d33704e8deebff91d88a24ce625d965776891de3
Jerzy Łoba <jloba@protonmail.com>
2023-05-22 02:16:13 +0200

    feat(gateway): initial cart and orders routes setup

--- service-gateway/src/routes/index.ts
+++ service-gateway/src/routes/index.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import { userRouter } from "./user";
 import { productsRouter } from "./products";
+import { cartRouter } from "./cart";
+import { ordersRouter } from "./orders";
+
+import { authMiddleware } from "../middlewares";
 
 const router = express.Router();
 
 router.use("/user", userRouter);
 router.use("/products", productsRouter);
+router.use("/cart", authMiddleware, cartRouter);
+router.use("/orders", authMiddleware, ordersRouter);
 router.use("/ping", (_req, res) => {
   // sanity check
   res.status(200).send("pong");
