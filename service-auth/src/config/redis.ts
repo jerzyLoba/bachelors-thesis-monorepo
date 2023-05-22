f7011cfb81472c0760e753b80eef0c6c67f0fc46
Jerzy Łoba <jloba@protonmail.com>
2023-05-22 02:10:34 +0200

    fix(auth): remove password to db for now

--- service-auth/src/config/redis.ts
+++ service-auth/src/config/redis.ts
@@ -6,8 +6,11 @@ const config = getConfig();
 
 export const client = createClient({
   url: `redis://${config.DB_HOST}:${config.DB_PORT}`,
+  // password: config.DB_PASSWORD,
 });
 client.on("connect", () => console.log("redis connected"));
-client.on("error", (err) => console.log("redis client error", err));
+client.on("error", (err) =>
+  console.log(`redis client error on ${config.DB_HOST}:${config.DB_PORT}`, err)
+);
 
 await client.connect();
