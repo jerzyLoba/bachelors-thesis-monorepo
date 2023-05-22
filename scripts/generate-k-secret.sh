5f8aad02650ba34629527d861c5c552c761f8389
Jerzy Łoba <jloba@protonmail.com>
2023-05-22 02:03:18 +0200

    chore: k8s secrets script and readme

---
+++ scripts/generate-k-secret.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+while getopts 's:' OPTION; do
+    case "$OPTION" in
+    s)
+        service_name=$OPTARG
+        ;;
+    ?)
+        echo "script usage: $(basename \$0) [-s service_name]" >&2
+        exit 1
+        ;;
+    *)
+        echo "Error in command line parsing"
+        exit 1
+        ;;
+    esac
+done
+
+shift "$(($OPTIND - 1))"
+
+if [ -z "$service_name" ]; then
+    echo 'Missing service name' >&2
+    exit 1
+fi
+
+status=$(minikube status -f "{{.Host}}")
+
+if [ "$status" = "Stopped" ]; then
+    echo "minikube stopped..."
+    echo "starting minikube"
+    minikube start
+fi
+
+echo $service_name
+cd ../$service_name
+kubectl create secret generic "${service_name}-secret" --from-env-file=.env
