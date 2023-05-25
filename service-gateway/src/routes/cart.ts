86bbc6906a65185ede421aa89fc80f0be2f5763e
Jerzy Łoba <jloba@protonmail.com>
2023-05-25 02:38:38 +0200

    feat(gateway): add cart and orders routing

--- service-gateway/src/routes/cart.ts
+++ service-gateway/src/routes/cart.ts
@@ -1,20 +1,81 @@
 import express from "express";
+import { AuthedRequest } from "../middlewares";
+import { cartServiceClient } from "../clients/cart";
+import { AddToCartRequest } from "../proto/inz/cart/AddToCartRequest";
+import { RemoveFromCartRequest } from "../proto/inz/cart/RemoveFromCartRequest";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/", (req: AuthedRequest, res: express.Response) => {
   try {
-  } catch (e) {}
+    const { user_id } = req.body;
+
+    cartServiceClient.GetCart({ user_id }, (err, response) => {
+      if (err) {
+        throw err;
+      }
+      console.log("service-cart: get cart from microservice ok");
+      console.log(response);
+
+      return res.status(200).send(response.cart);
+    });
+  } catch (e) {
+    console.log("service-gateway:cartServiceClient:GetCart:error", e);
+    return res.sendStatus(500);
+  }
 });
 
-router.patch("/", (req, res) => {
-  try {
-  } catch (e) {}
-});
+router.patch(
+  "/",
+  (req: AuthedRequest<{}, {}, AddToCartRequest>, res: express.Response) => {
+    try {
+      const { user_id, product_id, quantity } = req.body;
 
-router.delete("/", (req, res) => {
-  try {
-  } catch (e) {}
-});
+      cartServiceClient.AddToCart(
+        { user_id, product_id, quantity },
+        (err, response) => {
+          if (err) {
+            throw err;
+          }
+          console.log("service-cart: update cart from microservice ok");
+          console.log(response);
+
+          return res.status(200).send(response.cart);
+        }
+      );
+    } catch (e) {
+      console.log("service-gateway:cartServiceClient:AddToCart:error", e);
+      return res.sendStatus(500);
+    }
+  }
+);
+
+router.delete(
+  "/",
+  (
+    req: AuthedRequest<{}, {}, RemoveFromCartRequest>,
+    res: express.Response
+  ) => {
+    try {
+      const { user_id, product_id, quantity } = req.body;
+
+      cartServiceClient.RemoveFromCart(
+        { user_id, product_id, quantity },
+        (err, response) => {
+          if (err) {
+            throw err;
+          }
+          console.log("service-cart: remove from cart from microservice ok");
+          console.log(response);
+
+          return res.status(200).send(response.cart);
+        }
+      );
+    } catch (e) {
+      console.log("service-gateway:cartServiceClient:RemoveFromCart:error", e);
+      return res.sendStatus(500);
+    }
+  }
+);
 
 export { router as cartRouter };
--- service-gateway/src/routes/orders.ts
+++ service-gateway/src/routes/orders.ts
@@ -1,15 +1,44 @@
 import express from "express";
+import { orderServiceClient } from "../clients/orders";
+import { AuthedRequest } from "../middlewares";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/", (req: AuthedRequest, res: express.Response) => {
   try {
-  } catch (e) {}
+    const { user_id } = req.body;
+
+    orderServiceClient.GetUserOrders({ user_id }, (err, response) => {
+      if (err) {
+        throw err;
+      }
+      console.log("service-orders: get user orders from microservice ok");
+      console.log(response);
+
+      return res.status(200).send(response.orders);
+    });
+  } catch (e) {
+    console.log("service-gateway:ordersServiceClient:GetUserOrders:error", e);
+    return res.sendStatus(500);
+  }
 });
 
-router.post("/place", (req, res) => {
+router.post("/place", (req: AuthedRequest, res: express.Response) => {
   try {
-  } catch (e) {}
+    const { user_id } = req.body;
+    orderServiceClient.PlaceOrder({ user_id }, (err, response) => {
+      if (err) {
+        throw err;
+      }
+      console.log("service-orders: place order from microservice ok");
+      console.log(response);
+
+      return res.status(200).send(response.order);
+    });
+  } catch (e) {
+    console.log("service-gateway:ordersServiceClient:PlaceOrder:error", e);
+    return res.sendStatus(500);
+  }
 });
 
 export { router as ordersRouter };
