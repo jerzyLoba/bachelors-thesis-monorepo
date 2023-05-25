7f8568d45e4c2940d50d9c7e62a8e85bb4342efb
Jerzy Łoba <jloba@protonmail.com>
2023-05-25 02:24:05 +0200

    fix(orders): db schema

--- service-orders/scripts/create.sql
+++ service-orders/scripts/create.sql
@@ -1,20 +1,20 @@
-CREATE TYPE order_status AS ENUM (
+CREATE TYPE IF NOT EXISTS order_status AS ENUM (
     'PAYMENT_PENDING',
     'SENT',
     'FINISHED',
     'CANCELED',
     'UNDEFINED'
 );
-CREATE TYPE payment_method AS ENUM ('CREDIT_CARD', 'BANK_TRANSFER');
-CREATE TABLE orders(
+CREATE TYPE IF NOT EXISTS payment_method AS ENUM ('CREDIT_CARD', 'BANK_TRANSFER');
+CREATE TABLE IF NOT EXISTS orders(
     order_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_id INT NOT NULL,
     order_status VARCHAR DEFAULT 'UNDEFINED',
     payment_method VARCHAR DEFAULT 'BANK_TRANSFER',
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP,
+    updated_at TIMESTAMP
 );
-CREATE TABLE order_item(
+CREATE TABLE IF NOT EXISTS order_item(
     order_item_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     order_id INT,
     product_id INT NOT NULL,
