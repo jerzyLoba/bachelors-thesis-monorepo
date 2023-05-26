ecb09ea94339aa88815601741846db7d19874a0a
Jerzy Łoba <jloba@protonmail.com>
2023-05-26 02:08:26 +0200

    fix(cart): use redis-stack

--- service-cart/scripts/bootstrap-redis.sh
+++ service-cart/scripts/bootstrap-redis.sh
@@ -7,7 +7,8 @@ set +a
 docker run -p $DB_PORT:6379 -d \
     --name $DB_NAME \
     -v $DB_NAME:/data \
-    redis:7.0.7-alpine \
-    redis-server --save 60 1 \
+    redis/redis-stack-server:latest \
+    redis-stack-server --save 60 1 \
     --loglevel warning \
     --requirepass ${DB_PASSWORD}
+# TODO: add loadmodule instead of redis-stack
